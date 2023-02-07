c0cf0e56fcca5949bbfbe156bc61c455f8a47a76
pollardd <pollardd@gmail.com>
2023-02-07 20:29:07 +1100

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,8 +1,2 @@
-# Shared encryption key and Initialization Vector (IV)
-# These are sample keys only
-# Generate your keys and IV with GenerateEncryptionKeys.py
-# secrets.py deployed to all devices (Including Wifi)
-# secretsHouse.py deployed to all wifi device
-
-key=bytearray(b'"\xe6H\xc1\xb2\xb6\x8f\xdb%==\xea\x8a\x1a\xb5\xc5\x8f;\xf7r\x9e_E\xe1j\xcb\xd1\xbf\xaf\xbe9\xf9')
-IV=bytearray(b'\x7fy8y\x06t\xf9G\x1c%\x92\xbe\xb1\x00\xc6\xba')
+ssid = 'Set your Wifi SSID Here'
+password = 'Set your Wifi Password here'
