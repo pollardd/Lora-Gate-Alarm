7b377890da9b64bf7511887bce8d57b384d79025
pollardd <pollardd@gmail.com>
2023-02-07 20:10:23 +1100

    Update dateTime.py
    Update to Alpha release

--- dateTime.py
+++ dateTime.py
@@ -1,14 +1,27 @@
 import debug
 import time
+import constants
+
+# Constants
+DEBUG = constants.DEBUG
+LOGTOFILE =  constants.LOGTOFILE
+ENCRYPTION = constants.ENCRYPTION
+
 
 # Return time in HH:MM:SS format
 def formattedTime():
+    if(DEBUG >=2):
+        debug.debug(DEBUG, "dateTime.formattedTime()", " ", LOGTOFILE)
+
     now=time.localtime()
     timeString=zfl(str(now[3]),2) + ":" + zfl(str(now[4]),2) + ":" + zfl(str(now[5]),2)
     return timeString
 
 # Return Date in DD/MM/YYYY format
 def formattedDate():
+    if(DEBUG >=2):
+        debug.debug(DEBUG, "dateTime.formattedDate()", " ", LOGTOFILE)
+
     now=time.localtime()
     dateString=zfl(str(now[2]),2) + "/" + zfl(str(now[1]),2) + "/" + zfl(str(now[0]),2)
     return dateString
@@ -16,6 +29,8 @@ def formattedDate():
 # Zero pad the "imputString" with zeroes up to "width" for readability
 def zfl(inputString, width):
     # Zero padd the specified number of digits
+    if(DEBUG >=2):
+        debug.debug(DEBUG, "dateTime.zfl()", " ", LOGTOFILE)
+
     padded = '{:0>{w}}'.format(inputString, w=width)
     return padded
-
