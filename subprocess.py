48d128bb00c3b51ea29c1f2e2015ca05559322e8
pollardd <pollardd@gmail.com>
2023-02-07 19:55:36 +1100

    Update subprocess.py
    Update to the alpha release

--- subprocess.py
+++ subprocess.py
@@ -9,11 +9,7 @@ import time
 import machine
 import json
 import _thread
-
-# Receiving error
-# RuntimeError: maximum recursion depth exceeded
-# Tried this
-# _thread.stack_size(16 * 1024)
+import gc
 
 # Home baked imports
 import debug        # My debuging routine
@@ -22,6 +18,7 @@ import counters     # Counter variables shared between modules
 import dateTime     # My Date Time formatting routine
 import blink        # Reuseable code to flash the LED error messages or open gate count
 import loraMessage  # Standard encoding of json formatted message
+import encryption   # Encrypt and Decrypt routines
 
 # Constants
 DEBUG = constants.DEBUG
@@ -34,6 +31,7 @@ DSTDEVICE="MainGate"               #
 messageNumber=10000     # Sequential message number.  (mainGate.py starts at 1)
 gateOpen="False"        # mainHouse device does not monitor the gate
 batteryPercent=0        # mainHouse device is always connected to mains power supply
+previousHeartBeat=0     # Used to chech how long since the last Heart Beat was received
 
 # Pin Definitions
 ledPin = 5                              # Physical Pin 7  Gnd = 8
@@ -41,12 +39,15 @@ led = Pin(ledPin, Pin.OUT)              # Define pin as output
 #button =  Pin(14, Pin.IN, Pin.PULL_UP)  # Physical Pin 19 Gnd = 13
 buzzer = PWM(Pin(13))                   # Physical pin 17 Gnd = 18
 
+if(DEBUG >=1):
+    debug.debug(DEBUG, "Subprocess Init", "Pre Method Definitions" , LOGTOFILE)
+
 # Method Definitions
 # =========================================
 
 def playTone(frequency):
     if(DEBUG >=1):
-        debug.debug(DEBUG, "playTone(frequency)    ", "frequency=" + str(frequency), LOGTOFILE)
+        debug.debug(DEBUG, "playTone(frequency)", "frequency=" + str(frequency), LOGTOFILE)
 
     #  These values work on a speaker stolen out of a telephone handset
     #  You may need to play around with it to suit your requirements
@@ -79,27 +80,61 @@ def cb(events):
 
 def processMessage(msg,err):
     # Process the incoming message
-    if(DEBUG >=1):
-        debug.debug(DEBUG, "processMessage(msg,err)    ", "msg="+ str(msg), LOGTOFILE)
+        if(DEBUG >=1):
+	        debug.debug(DEBUG, "processMessage(msg,err)    ", "Message Received=" + str(msg), LOGTOFILE)
+		debug.debug(DEBUG, "processMessage(msg,err)    ", "msg type="+ str(type(msg)), LOGTOFILE)
+        if(DEBUG >=2):
+	     debug.debug(DEBUG, "processMessage(msg,err)    ", "err="+ str(err), LOGTOFILE)
+
+    decryptedMessage=encryption.decryptMessage(msg)     # Decrypt the message 
+    decodedMsg=decryptedMessage.decode()                # Decode the byte string into a string
     if(DEBUG >=2):
-        debug.debug(DEBUG, "processMessage(msg,err)    ", "msg type="+ str(type(msg)), LOGTOFILE)
-        debug.debug(DEBUG, "processMessage(msg,err)    ", "err="+ str(err), LOGTOFILE)
-
-    decryptedMessage=encryption.decryptMessage(msg)  # Decrypt the message 
-    decodedMsg=decryptedMessage.decode()              # Decode the byte string into a string
-
-    if(DEBUG >=2):
-        debug.debug(DEBUG, "processMessage(msg,err)    ", "decodedMsg="+ str(decodeMsg), LOGTOFILE)
-        debug.debug(DEBUG, "processMessage(msg,err)    ", "decodedMsg type="+ str(type(decodedMsg)), LOGTOFILE)
-
-    jsonDict=json.loads(decodedMsg)  # Load string into a json object
+        debug.debug(DEBUG, "processMessage(msg,err)    ", "decodedMsg="+ str(decodedMsg), LOGTOFILE)
+    if(DEBUG >=1):    
+        debug.debug(DEBUG, "processMessage(msg,err)    ", "decodedMsg type="+ str(type(decodedMsg)), LOGTOFILE)   
+    # Load incoming message into a json object
+    jsonDict=json.loads(decodedMsg)
     if(DEBUG >=2):
         debug.debug(DEBUG, "processMessage(msg,err)    ", "jsonDict=" + str(jsonDict), LOGTOFILE)
         debug.debug(DEBUG, "processMessage(msg,err)    ", "jsonDict type=" + str(type(jsonDict)), LOGTOFILE)
 
-    compareTimeStamps(jsonDict)
-    checkGateStatus(jsonDict)
+    # Check destination of this message = this device
+    # LoRa messages are broadcast and any device can pick them up
 
+    destination = jsonDict["DstDevice"]
+    
+    if(DEBUG >=1):
+        debug.debug(DEBUG, "processMessage(msg,err)", "destination="+destination, LOGTOFILE)
+        debug.debug(DEBUG, "processMessage(msg,err)", "DSTDEVICE="+DSTDEVICE, LOGTOFILE)
+
+    # Don't forget the device SRCDEVICE is the name of this machine (receiving the message)
+    if(destination == SRCSTDEVICE): 
+        compareTimeStamps(jsonDict)
+        checkGateStatus(jsonDict)
+        checkHeartBeat(jsonDict)
+        
+def checkHeartBeat(jsonDict):
+    if(DEBUG >=1):
+        debug.debug(DEBUG, "cHeckHeartBeat(jsonDict)", " ", LOGTOFILE)
+
+    # 'TextMessage': 'Heart Beat',
+
+    textMessage = jsonDict["TextMessage"] 
+    if(textMessage=="Heart Beat"):
+        # Get current hour
+        now=time.localtime()
+        #  Get the current hour
+        hour=int(now[3])
+
+        # If Previous Hour = 0
+            # Save current hour as previous hour
+        # Else
+            # if Current hour >= Previous hour +1
+                # Save current hour as previous hour
+            # esle
+                # display error message
+                # 1 long, 5 short = Heartbeat message timed out
+                
 def compareTimeStamps(jsonDict):
     if(DEBUG >=1):
         debug.debug(DEBUG, "compareTimeStamps(jsonDict)    ", "jsonDict="+ str(jsonDict), LOGTOFILE)
@@ -131,12 +166,12 @@ def checkGateStatus(jsonDict):
         debug.debug(DEBUG, "checkGateStatus(jsonDict)    ", "jsonDict="+ str(jsonDict), LOGTOFILE)
 
     gateOpen=jsonDict["GateOpen"]    # Read the GateOpen: string out of the json message
-    if(DEBUG >=2):
+    if(DEBUG >=1):
         debug.debug(DEBUG, "checkGateStatus(jsonDict)    ", "gateOpen="+ str(gateOpen), LOGTOFILE)
 
     # If open play tone
     if(gateOpen=="True"):
-        playTone(HZ)
+        playTone(constants.HZ)
         stopTone()
         # Count the number of gate open events
         counters.openCount = counters.openCount+1
@@ -167,14 +202,21 @@ def sendTimeUpdate():
 # End of method Definitions.
 
 def subMain():
+    #print("main() Subprocess mem before gc="+ str(gc.mem_free()))
+    gc.collect()
+    #print("main() Subprocess mem after gc="+ str(gc.mem_free()))
+
     if(DEBUG >=1):
         debug.debug(DEBUG, "subprocess.subMain()", "Start....", LOGTOFILE)
 
     loraMessage.sx.setBlockingCallback(False, cb)
     while True:
         if(counters.openCount>0):        # Show the number of gate open events
-            flash(0,counters.openCount)
+            blink.flash(mainHouse.ledpin, 0,counters.openCount)
         if(DEBUG >=1):
             debug.debug(DEBUG, "subprocess.subMain()", "openCount="+ str(counters.openCount) , LOGTOFILE)
 
-        blink.checkButtonPress(2)
+        blink.checkButtonPress(5)
+
+if(DEBUG >=1):
+    debug.debug(DEBUG, "Subprocess Init", "End Method Definitions" , LOGTOFILE)
