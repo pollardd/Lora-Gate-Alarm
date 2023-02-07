27b2a985a606d9296ea67a398c27b37d9dbda63c
pollardd <pollardd@gmail.com>
2023-02-07 20:02:18 +1100

    Update encryption.py
    Updates to alpha release

--- encryption.py
+++ encryption.py
@@ -9,32 +9,39 @@ DEBUG = constants.DEBUG
 LOGTOFILE =  constants.LOGTOFILE
 
 def encryptMessage(unencryptedMessage):
-    if(DEBUG >=2):
-        debug.debug(DEBUG, "encryptMessage(unencryptedMessage)    ", "unencryptedMessage="+ str(unencryptedMessage), LOGTOFILE)
     if(DEBUG >=1):
-        debug.debug(DEBUG, "encryptMessage(unencryptedMessage)    ", "unencryptedMessage Length ="+ str(len(str(unencryptedMessage))), LOGTOFILE)
+        debug.debug(DEBUG, "encryptMessage()", " ", LOGTOFILE)
+
+    if(DEBUG >=2):
+        debug.debug(DEBUG, "encryptMessage(unencryptedMessage)", "unencryptedMessage="+ str(unencryptedMessage), LOGTOFILE)
+    if(DEBUG >=2):
+        debug.debug(DEBUG, "encryptMessage(unencryptedMessage)", "unencryptedMessage Length ="+ str(len(str(unencryptedMessage))), LOGTOFILE)
 
     key = secrets.key
     IV = secrets.IV
     aes = mpyaes.new(key, mpyaes.MODE_CBC, IV)
     encryptedMessage = aes.encrypt(unencryptedMessage)
 
+    if(DEBUG >=3):
+        debug.debug(DEBUG, "encryptMessage(unencryptedMessage)", "encryptedMessage="+ str(encryptedMessage), LOGTOFILE)
     if(DEBUG >=2):
-        debug.debug(DEBUG, "encryptMessage(unencryptedMessage)    ", "encryptedMessage="+ str(encryptedMessage), LOGTOFILE)
-    if(DEBUG >=1):
-        debug.debug(DEBUG, "encryptMessage(unencryptedMessage)    ", "encryptedMessage Length="+ str(len(str(encryptedMessage))), LOGTOFILE)
+        debug.debug(DEBUG, "encryptMessage(unencryptedMessage)", "encryptedMessage Length="+ str(len(str(encryptedMessage))), LOGTOFILE)
 
     return encryptedMessage
 
 def decryptMessage(encryptedMessage):
-    if(DEBUG >=2):
-        debug.debug(DEBUG, "decryptMessage(encryptedMessage)    ", "encryptedMessage="+ str(encryptedMessage), LOGTOFILE)
     if(DEBUG >=1):
-        debug.debug(DEBUG, "decryptMessage(encryptedMessage)    ", "encryptedMessage Length="+ str(len(str(encryptedMessage))), LOGTOFILE)
+        debug.debug(DEBUG, "decryptMessage()", " ", LOGTOFILE)
+    if(DEBUG >=3):
+        debug.debug(DEBUG, "decryptMessage(encryptedMessage)", "encryptedMessage="+ str(encryptedMessage), LOGTOFILE)
+    if(DEBUG >=2):
+        debug.debug(DEBUG, "decryptMessage(encryptedMessage)", "encryptedMessage Length="+ str(len(str(encryptedMessage))), LOGTOFILE)
 
     key = secrets.key
     IV = secrets.IV
     aes = mpyaes.new(key, mpyaes.MODE_CBC, IV)
     unencryptedMessage = aes.decrypt(bytearray(encryptedMessage))
+    if(DEBUG >=1):
+        debug.debug(DEBUG, "decryptMessage(encryptedMessage)", "decrypted message="+ str(unencryptedMessage), LOGTOFILE)
 
     return unencryptedMessage
