fe7fbc5aee94b2f8ea96ca8e4cc1f650851fe32d
pollardd <pollardd@gmail.com>
2023-02-15 21:28:28 +1100

    Update constants.py
    Added new fields for sounds

--- constants.py
+++ constants.py
@@ -3,4 +3,12 @@ LOGTOFILE = True
 ENCRYPTION = True
 TIMEZONE = +10
 NTPSERVER = "au.pool.ntp.org"
-HZ=900                             # Tone of the sound
+MAXHEARTBEAT = 3600              # 1 hour in seconds
+MINBATTERY = 5                   # Minimum Battery Percantage
+#
+# Sound Values
+LOWFREQUENCY = 900               # Low Tone Frequency in HZ
+HIGHFREQUENCY = 1100             # High Tone Frequency in HZ
+DELAY = 1                        # Lenght of tone
+REPEAT = 5                       # How may times to play 
+VOLUME = 40000                   # 40000 seems to be maximum
