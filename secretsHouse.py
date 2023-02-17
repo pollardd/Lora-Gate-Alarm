86224a802ca46f3d562ec5c63bd878a661f75d07
pollardd <pollardd@gmail.com>
2023-02-17 18:22:02 +1100

    Update secretsHouse.py
    split into two files.  secrets.py for encryption and secretsHouse.py for wifi

--- secretsHouse.py
+++ secretsHouse.py
@@ -1,10 +1,2 @@
 ssid = 'Set your Wifi SSID Here in secretsHouse.py'
 password = 'Set your Wifi Password here'
-
-# Shared encryption key and Initialization Vector (IV)
-# Generate your own with GenerateEncryptionKyeys.py
-# The same keys are used to encrypt and decrypt.
-# Share the same keys between all trusted devices
-
-key=bytearray(b'"\xe6H\xc1\xb2\xb6\x8f\xdb%==\xea\x8a\x1a\xb5\xc5\x8f;\xf7r\x9e_E\xe1j\xcb\xd1\xbf\xaf\xbe9\xf9')
-IV=bytearray(b'\x7fy8y\x06t\xf9G\x1c%\x92\xbe\xb1\x00\xc6\xba')
