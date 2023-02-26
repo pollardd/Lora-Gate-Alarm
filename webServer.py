302e4f9454d3a5e07f95be65993770f0421f8630
pollardd <pollardd@gmail.com>
2023-02-26 21:10:05 +1100

    Update webServer.py
    Added Gate voltage percentage and time recorded to web page

--- webServer.py
+++ webServer.py
@@ -13,10 +13,12 @@ import time
 import debug        # My debuging routine
 import constants
 import counters
+import dateTime
 
 # Constants
 DEBUG = constants.DEBUG
 LOGTOFILE =  constants.LOGTOFILE
+mainGateFileName="batteryLevel.txt"
 
 # Temperature Sensor
 sensor_temp = machine.ADC(4)
@@ -45,7 +47,7 @@ def temperature():
     time.sleep(1)
     return temperature_Celcius
  
-def webpage(value):
+def webpage(tempValue, batteryPercentage, batteryUpdateTime):
     html = f"""
             <!DOCTYPE html>
             <html>
@@ -56,7 +58,9 @@ def webpage(value):
             <form action="./off">
             <input type="submit" value="Led Off" />
             </form>
-            <p>Temperature is {value} degrees Celsius</p>
+            <p>House Pico Temperature is {tempValue} degrees Celsius</p>
+            <p>Gate Battery Percentage is {batteryPercentage}</p>
+            <p>Gate Battery Update Time is {batteryUpdateTime}</p>
             </body>
             </html>
             """
@@ -65,40 +69,62 @@ def webpage(value):
 def serve(connection):
     while True:
         if(DEBUG >=1):
-            debug.debug(DEBUG, "webServer.server(connection)", "Wait for web request",LOGTOFILE)
+            debug.debug(DEBUG, "webServer.serve(connection)", "Wait for web request",LOGTOFILE)
         client = connection.accept()[0]
         if(DEBUG >=2):
-            debug.debug(DEBUG, "webServer.server(connection)", "client="+str(client),LOGTOFILE)
+            debug.debug(DEBUG, "webServer.serve(connection)", "client="+str(client),LOGTOFILE)
         request = client.recv(1024)
         if(DEBUG >=2):
-            debug.debug(DEBUG, "webServer.server(connection)", "request="+str(request),LOGTOFILE)
+            debug.debug(DEBUG, "webServer.serve(connection)", "request="+str(request),LOGTOFILE)
         request = str(request)
         try:
             request = request.split()[1]
             if(DEBUG >=1):
-                debug.debug(DEBUG, "webServer.server(connection)", "request.split()[1]="+str(request),LOGTOFILE)
+                debug.debug(DEBUG, "webServer.serve(connection)", "request.split()[1]="+str(request),LOGTOFILE)
 
         except IndexError:
             if(DEBUG >=1):
-                debug.debug(DEBUG, "webServer.server(connection)", "IndexError",LOGTOFILE)
+                debug.debug(DEBUG, "webServer.serve(connection)", "IndexError",LOGTOFILE)
             pass
         
         if request == '/off?':
             led.low()
             if(DEBUG >=1):
-                debug.debug(DEBUG, "webServer.server(connection)", "set led off",LOGTOFILE)
+                debug.debug(DEBUG, "webServer.serve(connection)", "set led off",LOGTOFILE)
 
         elif request == '/led?':
             led.high()
             if(DEBUG >=1):
-                debug.debug(DEBUG, "webServer.server(connection)", "set led on",LOGTOFILE)
- 
-        value='%.2f'%temperature()    
-        html=webpage(value)
+                debug.debug(DEBUG, "webServer.serve(connection)", "set led on",LOGTOFILE)
+        #Get the Temperature
+        tempValue='%.2f'%temperature()
+
+        # Read the most recent Main Gate Battery values from a file
+        fileHandle=open(mainGateFileName,"r")
+        batteryPercentage=fileHandle.readline()
+        batteryUpdateTimeTuple=eval(str(fileHandle.readline()))
+        if(DEBUG >=1):
+            debug.debug(DEBUG, "webServer.serve(connection)", "batteryUpdateTimeTuple="+ str(batteryUpdateTimeTuple),LOGTOFILE)
+
+        batteryUpdateTime=str(dateTime.formattedTimeString(batteryUpdateTimeTuple))
+        if(DEBUG >=1):
+            debug.debug(DEBUG, "webServer.serve(connection)", "batteryUpdateTime="+ batteryUpdateTime,LOGTOFILE)
+
+        batteryUpdateDate=str(dateTime.formattedDateString(batteryUpdateTimeTuple))
+        if(DEBUG >=1):
+            debug.debug(DEBUG, "webServer.serve(connection)", "batteryUpdateDate="+ batteryUpdateDate,LOGTOFILE)
+
+        fileHandle.close()
+        
+        html=webpage(tempValue, batteryPercentage, batteryUpdateDate + " " + batteryUpdateTime)
         client.send(html)
         client.close()
  
 def open_socket(ip):
+    #<TODO> Add a try catch block here
+    # Received this error "OSError: [Errno 98] EADDRINUSE"
+    # On the connection.bind(address) line below
+    
     # Open a socket
     if(DEBUG >=1):
         debug.debug(DEBUG, "webServer.open_socket(ip) ", "Open a socket",LOGTOFILE)
